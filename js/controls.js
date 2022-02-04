b59f7cadfc512f7ee322609a6d21fd428142e4d7
Błażej Niewiadomski <blaz.nie@protonmail.com>
2022-02-04 01:15:47 +0100

    made a basic website

---
+++ js/controls.js
@@ -0,0 +1,23 @@
+
+
+// const body = document.getElementsByTagName("body")[0]
+// const bgColorSlider = document.getElementById("bg-color-slider")
+// const textColorSlider = document.getElementById("text-color-slider")
+
+// const computeColor = (value) =>
+//       Math
+//       .floor(value * 167772.16)
+//       .toString(16)
+//       .padStart(6, '0')
+
+// const setBackgroundColor = (event) => {
+//   body.style.backgroundColor = `#${computeColor(event.target.value)}`
+// }
+
+// const setTextColor = (event) => {
+//   body.style.color = `#${computeColor(event.target.value)}`
+//   console.log(`#${computeColor(event.target.value)}`)
+// }
+
+// bgColorSlider.addEventListener("input", setBackgroundColor)
+// textColorSlider.addEventListener("input", setTextColor)
