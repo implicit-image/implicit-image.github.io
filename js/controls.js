490814a79e4b6e9d80e10efceb7e462bf844aab7
Błażej Niewiadomski <blaz.nie@protonmail.com>
2023-04-24 15:07:19 +0200

    added cursor fiollower code and removed slider

--- js/controls.js
+++ js/controls.js
@@ -1,22 +1,23 @@
 
 
-const body = document.getElementsByTagName("body")[0]
-// const bgColorSlider = document.getElementById("bg-color-slider")
-const textColorSlider = document.getElementById("text-color-slider")
-const testButton = document.getElementById("test-button")
-console.log(testButton)
+const body = document.getElementsByTagName("body")[0];
+// const bgColorSlider = document.getElementById("bg-color-slider");
+const textColorSlider = document.getElementById("text-color-slider");
+const testButton = document.getElementById("test-button");
+console.log(testButton);
 
 const computeColor = (value) =>
       Math
       .floor(value * 167772.16)
       .toStrig(16)
-      .padStart(6, '0')
+      .padStart(6, '0');
 
 
 const setBackgroundColor = (event) => {
-  body.setAttribute('style', `color: red`)
+  body.setAttribute('style', `color: red`);
 }
 
 
 
-testButton.addEventListener("click", setBackgroundColor)
+testButton.addEventListener("click", setBackgroundColor);
+
